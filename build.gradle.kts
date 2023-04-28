9faa9d88fa8d82479f68143209fe936585eac449
Riley Park <rileysebastianpark@gmail.com>
2023-04-28 04:47:46 -0700

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
   val indraVersion = "3.0.1"
-  id("com.diffplug.spotless") version "6.17.0"
+  id("com.diffplug.spotless") version "6.18.0"
   id("net.kyori.indra") version indraVersion
   id("net.kyori.indra.checkstyle") version indraVersion
   id("net.kyori.indra.publishing") version indraVersion
   id("net.kyori.indra.publishing.sonatype") version indraVersion
-  id("net.ltgt.errorprone") version "3.0.1"
+  id("net.ltgt.errorprone") version "3.1.0"
 }
 
 group = "com.seiama"
@@ -64,7 +64,7 @@ dependencies {
   errorprone("com.google.errorprone:error_prone_core:2.18.0")
   compileOnlyApi("org.jetbrains:annotations:24.0.1")
   testImplementation("com.google.guava:guava-testlib:31.1-jre")
-  testImplementation(platform("org.junit:junit-bom:5.9.2"))
+  testImplementation(platform("org.junit:junit-bom:5.9.3"))
   testImplementation("org.junit.jupiter:junit-jupiter-api")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
